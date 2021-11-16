edb06d500e636f812485c5932fd579e037cc21ac
Aaron Sando <Aaron@Charactercabinets.com.au>
2021-11-16 12:08:58 +0800

    updated columns

--- models/index.js
+++ models/index.js
@@ -4,13 +4,36 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
 // Categories have many Products
+// Define a Category as having many Products, thus creating a foreign key in the `Product` table
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
+
+// Products belongsTo Category
+// The association can also be created from the Product side
+Product.belongsTo(Category, {
+  foreignKey: 'Category_id',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { 
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'products_tag' 
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { 
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tagged_products' 
+});
 
 module.exports = {
   Product,
